215ea079a8bb07a0c194b1f30e54e3451da07673
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-07 15:31:13 -0500

    primitive practise finished

---
+++ primitive_practise.js
@@ -0,0 +1,30 @@
+<tilte>Primitive type Exercise</title>
+/* 99.9 -> number
+
+"false" -> string
+
+false -> boolean
+
+'0' -> string
+
+0 -> number
+
+true -> boolean
+
+'true' -> string */
+
+//==================//
+   /*
+    A term or phrase typed into a search box?
+    string
+    If a user is logged in?
+    boolean
+    A discount amount to apply to a user's shopping cart?
+    number
+    Whether or not a coupon code is valid?
+    boolean
+    An email address typed into a registration form?
+    string
+    The price of a product?
+    number
+    */
