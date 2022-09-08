c8df9493d6b37517004add6d126b0994d00d34c7
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-07 21:23:09 -0500

    finished-operators_practise

---
+++ operators_practise.js
@@ -0,0 +1,64 @@
+<title>operators_practise</tiitle>
+/*
+'1' + 2
+prediction:3
+outcome:12 ,the value of the string concatinates with the number
+
+typeof '1' + 2
+prediction:3
+outcome:string2, 'string' concatinates with 2.
+
+6 % 4
+prediction:2
+outcome:2, The remainder of 6/4 is 2.
+
+'3 + 4 is ' + 3 + 4
+prediction:3 + 4 is 12
+outcome:'3 + 4 is 34', the string '3 + 4 is ', is
+concatinated with 34
+
+// how could you make this produce the correct output?//
+
+0 < 0
+answer:0 <= 0.
+
+'false' == false
+answer:'false' !== false
+
+true == 'true'
+answer:true !== 'true'
+
+5 >= -5
+answer:true
+
+!false || false
+answer:true
+
+true || "42"
+answer:true
+
+!true && !false
+answer:false
+
+6 % 5
+answer:1
+
+5 < 4 && 1 === 1
+answer:false
+
+typeof 'codeup' === 'string'
+answer:true
+
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+answer:false
+
+4 >= 0 && 1 !== '1'
+answer:true
+
+6 % 3 === 0
+answer:true
+
+5 % 2 !== 0
+answer:true
+
+*/
