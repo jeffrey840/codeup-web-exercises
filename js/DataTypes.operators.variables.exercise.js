56090b8388ae73e5a2d5b17f9b28376a08d2a225
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-08 16:00:20 -0500

    organized files

---
+++ js/DataTypes.operators.variables.exercise.js
@@ -0,0 +1,79 @@
+<title>Data Types, Operators, and Variables </titile>
+/*
+var a = 1;  answer:1
+var b = a++;  answer:  the original console output is undefined , b is then called again and assigned 1.
+var c = ++a; answer: the origin output is undefined, c is then called again and is immediately assigned 3 before "enter"
+// what is the value of a, b, and c? answer:a=1, b=1, c=3
+
+var d = "hello"; answer: 'hello'
+var e = false; answer: 0
+
+d++; answer:NaN
+e++; answer:
+
+var perplexed; // perplexed is undefined (no value is assigned)
+perplexed + 2; answer:
+
+var price = 2.7; answer:
+price.toFixed(2); answer:
+
+var price = "2.7"; answer:
+price.toFixed(2); answer:
+
+isNaN(0) answer:
+
+isNaN(1) answer:
+
+isNaN("") answer:
+
+isNaN("string") answer:
+
+isNaN("0") answer:
+
+isNaN("1") answer:
+
+isNaN("3.145") answer:
+
+isNaN(Number.MAX_VALUE) answer:
+
+isNaN(Infinity) answer:
+
+isNaN("true") answer:
+
+isNaN(true) answer:
+
+isNaN("false") answer:
+
+isNaN(false) answer:
+
+// to illustrate why the isNaN() function is needed:
+NaN == NaN answer:
+
+!true answer:
+
+!false answer:
+
+!!true answer:
+
+!!false answer:
+
+!!0 answer:
+
+!!-0 answer:
+
+!!1 answer:
+
+!!-1 answer:
+
+!!0.1 answer:
+
+!!"hello" answer:
+
+!!"" answer:
+
+!!'' answer:
+
+!!"false" answer:
+
+!!"0" answer:
+*/
