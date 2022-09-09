2ae234172e162c73fb75b758f23e54352d06f90c
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-09 15:25:45 -0500

    deleted some multi-line comments

--- js/external_js.js
+++ js/external_js.js
@@ -8,14 +8,15 @@ alert(userInput + " is my favorite color too!");
 
  */
 
-/* question 1
+/*
+// question 1
 var userHours = prompt('how many hours did you work today?')
 var userPay = prompt('how much do you get paid per hour')
 var totalPay = userHours * userPay;
 alert('you got paid ' + totalPay + '$');
-*/
 
-/*
+
+//question 2
 var userInputPerRental = prompt('what was the price for a movie per day')
 var rentalPrice = userInputPerRental;
 
@@ -25,19 +26,20 @@ let herculesMovie = prompt("how long did you rent herculesMovie for?")
 
 let totalPrice = (brotherBear * rentalPrice) + (littleMermaid * rentalPrice) + (herculesMovie * rentalPrice);
 alert('you spend ' + totalPrice + ' $ on movies')
-*/
 
-/*
+
+//question 3
 var spaceInClass= confirm('Does this classroom have space?')
 var spaceInSchedule = confirm('Can your schedule support this class?');
 var canBeEnrolled = spaceInSchedule && spaceInClass;
 alert('Can you enroll? ' + canBeEnrolled);
-*/
 
-/*
+
+//question 4
 var moreThan2 = confirm('Did you buy more than two items?');
 var expired = confirm('Is the coupon valid?');
 var premium = confirm('are you a premium member?');
 var couponApplied = expired && (moreThan2 || premium);
 alert('This coupon is ,result:' + couponApplied);
-*/
+
+ */
