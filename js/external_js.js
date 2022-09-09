2c784025f89e35cc2f016acd8e84fceb90a1dff4
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-09 15:57:46 -0500

    switched questions 1 and 2

--- js/external_js.js
+++ js/external_js.js
@@ -3,37 +3,36 @@ console.log("hello from external javascript")
 
 alert('welcome to my website');
 
-var userInput = prompt('whats your favorite color')
+var userInput = prompt("whats your favorite color");
 alert(userInput + " is my favorite color too!");
-
- */
+*/
 
 /*
 // question 1
-var userHours = prompt('how many hours did you work today?')
-var userPay = prompt('how much do you get paid per hour')
-var totalPay = userHours * userPay;
-alert('you got paid ' + totalPay + '$');
-
-
-//question 2
-var userInputPerRental = prompt('what was the price for a movie per day')
+var userInputPerRental = prompt('what was the price for a movie per day');
 var rentalPrice = userInputPerRental;
 
-let brotherBear = prompt("how long did you rent BrotherBear for?")
-let littleMermaid = prompt("how long did you rent littleMermaid for?")
-let herculesMovie = prompt("how long did you rent herculesMovie for?")
+let brotherBear = prompt("how long did you rent BrotherBear for?");
+let littleMermaid = prompt("how long did you rent littleMermaid for?");
+let herculesMovie = prompt("how long did you rent herculesMovie for?");
 
 let totalPrice = (brotherBear * rentalPrice) + (littleMermaid * rentalPrice) + (herculesMovie * rentalPrice);
-alert('you spend ' + totalPrice + ' $ on movies')
+alert('you spend ' + totalPrice + ' $ on movies');
 
+//question 2
+let google = Number(prompt('how many hours did you work at google this week?'));
+let googlePay= 400;
+let amazon = Number(prompt('how many hours did you work at amazon this week?'));
+let amazonPay= 380;
+let Meta = Number(prompt('how many hours did you work at Meta this week?'));
+let metaPay= 350;
+alert('Your paycheck is:' + ((google * googlePay) + (amazon * amazonPay) + (Meta * metaPay)));
 
 //question 3
-var spaceInClass= confirm('Does this classroom have space?')
+var spaceInClass= confirm('Does this classroom have space?');
 var spaceInSchedule = confirm('Can your schedule support this class?');
 var canBeEnrolled = spaceInSchedule && spaceInClass;
 alert('Can you enroll? ' + canBeEnrolled);
-
 
 //question 4
 var moreThan2 = confirm('Did you buy more than two items?');
@@ -41,5 +40,4 @@ var expired = confirm('Is the coupon valid?');
 var premium = confirm('are you a premium member?');
 var couponApplied = expired && (moreThan2 || premium);
 alert('This coupon is ,result:' + couponApplied);
-
  */
