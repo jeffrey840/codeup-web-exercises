2e80eb2b150992acef9a503186f4f9ad5d1b6727
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-09 15:05:45 -0500

    finished javascipt/Html exercise

---
+++ js/external_js.js
@@ -0,0 +1,43 @@
+/*
+console.log("hello from external javascript")
+
+alert('welcome to my website');
+
+var userInput = prompt('whats your favorite color')
+alert(userInput + " is my favorite color too!");
+
+ */
+
+/* question 1
+var userHours = prompt('how many hours did you work today?')
+var userPay = prompt('how much do you get paid per hour')
+var totalPay = userHours * userPay;
+alert('you got paid ' + totalPay + '$');
+*/
+
+/*
+var userInputPerRental = prompt('what was the price for a movie per day')
+var rentalPrice = userInputPerRental;
+
+let brotherBear = prompt("how long did you rent BrotherBear for?")
+let littleMermaid = prompt("how long did you rent littleMermaid for?")
+let herculesMovie = prompt("how long did you rent herculesMovie for?")
+
+let totalPrice = (brotherBear * rentalPrice) + (littleMermaid * rentalPrice) + (herculesMovie * rentalPrice);
+alert('you spend ' + totalPrice + ' $ on movies')
+*/
+
+/*
+var spaceInClass= confirm('Does this classroom have space?')
+var spaceInSchedule = confirm('Can your schedule support this class?');
+var canBeEnrolled = spaceInSchedule && spaceInClass;
+alert('Can you enroll? ' + canBeEnrolled);
+*/
+
+/*
+var moreThan2 = confirm('Did you buy more than two items?');
+var expired = confirm('Is the coupon valid?');
+var premium = confirm('are you a premium member?');
+var couponApplied = expired && (moreThan2 || premium);
+alert('This coupon is ,result:' + couponApplied);
+*/
