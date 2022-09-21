4c79e664f7d03262e8c4886f176e37c03b555e3f
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-21 11:12:59 -0500

    finished warm up exercises

--- js/Warm-up-Exercises.js
+++ js/Warm-up-Exercises.js
@@ -1,3 +1,5 @@
+
+//question 1
 // Write a JavaScript function that accepts the 3 lengths of a triangle and outputs the triangle’s area.
 //     Reverse a String: Write a JS function that accepts a string and returns the string in reverse. example input: codeup, example output: puedoc
 
@@ -12,7 +14,7 @@ function area(a, b, c) {
 }
 // area(1,2,55,)
 
-
+//question 2
 //must be string
 //reverse string
 //ex: codeup/puedoc
@@ -26,6 +28,7 @@ function reverse(x) {
 }
 // reverse('reverse string')
 
+//question 3
 // Print numbers 1-100. If # is divisible by 3 print Fizz. If # is
 // divisible by 5 print Buzz. If # is divisible by both 3 and 5 print FizzBuzz
 
@@ -34,10 +37,44 @@ function reverse(x) {
 //if div by 5 :print buzz
 // if div by 3 and 5: fizz buzz
 
-let randomNumber = Math.ceil(Math.random() * 100 + 1)
-function FB(randomNumber) {
-
-    if (randomNumber % 3 === 0 && randomNumber % 5 === 0){
-        console.log(`{$randomNumber} is fizz buzz`)
+// let randomNumber = Math.ceil(Math.random() * 100 + 1)
+// for (let i = 0; i < 11; i++) {
+//     console.log(i)
+// }
+    for (let randomNumber = 1; randomNumber < 100; randomNumber++) {
+        if (randomNumber % 3 === 0 && randomNumber % 5 === 0){
+            console.log(randomNumber + " is fizz buzz")
+        }else if (randomNumber % 3 === 0) {
+            console.log(randomNumber + " is fizz")
+        }else if (randomNumber % 3 === 5) {
+            console.log(randomNumber + " is buzz")
+        }else { console.log(randomNumber)}
     }
+
+//question 4
+//Write a function to shuffle an array to a completely and entirely random order.
+
+//must  be function
+//make a random variable
+// let randomNumber = Math.ceil(Math.random() * 50 + 1)
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
+
+let arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr);
