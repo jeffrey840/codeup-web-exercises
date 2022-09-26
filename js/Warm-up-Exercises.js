2d8920c6ed2ccfbb8270b2c970c0bef4f0ab0ded
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-26 09:35:50 -0500

    commiting warm-up

--- js/Warm-up-Exercises.js
+++ js/Warm-up-Exercises.js
@@ -109,6 +109,32 @@ function sorting (Array) {
 sorting(library)
 
 
+// / Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
+// // example input: [1,2,3,4,5] expected output: [2,4]
+//
+// let input = [1,2,3,4,5]
+//
+// function evenNum(input) {
+//     let result = input.filter(x => x % 2 == 0)
+//     console.log(result);
+// }
+// console.log(evenNum(input))
+//
+// // Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+//
+// //     example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+// // expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+//
+// function addToArr(arr, key) {
+//     arr.map(key => ({...key, continent: 'Asia'}))
+// }
+// console.log(addToArr());
+
+
+
+
+
+
 
 
 
