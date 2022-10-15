a0481de8c27c4a9a1d7e816a297e5e3deb4100a2
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-14 22:43:52 -0500

    added new searchbar

--- ignore/javascript/searchbar.js
+++ ignore/javascript/searchbar.js
@@ -1,31 +1,22 @@
 
 
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
-const searchInput = document.querySelector("[data-search]")
 
-let users = []
+//g4g start
 
-searchInput.addEventListener("input", e => {
-	const value = e.target.value.toLowerCase()
-	users.forEach(user => {
-		const isVisible =
-			user.name.toLowerCase().includes(value) ||
-			user.email.toLowerCase().includes(value)
-		user.element.classList.toggle("hide", !isVisible)
-	})
-})
+// JavaScript code
+function search_animal() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('animals');
 
-fetch("https://jsonplaceholder.typicode.com/users")
-	.then(res => res.json())
-	.then(data => {
-		users = data.map(user => {
-			const card = userCardTemplate.content.cloneNode(true).children[0]
-			const header = card.querySelector("[data-header]")
-			const body = card.querySelector("[data-body]")
-			header.textContent = user.name
-			body.textContent = user.email
-			userCardContainer.append(card)
-			return { name: user.name, email: user.email, element: card }
-		})
-	})
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";
+		}
+	}
+}
+
+//g4g end
