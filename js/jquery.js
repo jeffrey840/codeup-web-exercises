b1800531d48b2ff3a0e0875cf7ff013e30f3c0da
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-20 11:52:01 -0500

    added konami konami solution

--- js/jquery.js
+++ js/jquery.js
@@ -3,3 +3,18 @@
 $(document).keyup(function (event) {
 			console.log(event.keyCode);
 		});
+
+let kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+
+$(document).keydown(function(e) {
+
+	kkeys.push( e.keyCode );
+
+	if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+
+		$(document).unbind('keydown');
+
+		alert("cheatcode activated")
+	}
+
+});
