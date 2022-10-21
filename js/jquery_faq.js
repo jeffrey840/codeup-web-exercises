635f3e0e27d2469ae1e7e4f31ec1c3b36ca4b4b1
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-21 10:56:07 -0500

    added comments

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,9 +1,14 @@
 
-
+//when the element with the id "toggle-c" is clicked, the dd element with class invisible will be toggled
 $( "#toggle-c" ).click(function() {
 	$("dd" ).toggleClass( "invisible" );
 });
 
+//when clicked the dt element will turn yellow
 $( "dt" ).click(function() {
 	$( this ).css( "background-color", "yellow" )
 });
+
+
+
+$('li').last().css('background-color', '#FF0');
