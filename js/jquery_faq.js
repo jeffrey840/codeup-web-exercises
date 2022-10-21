aaba86de063ddabc6e92b40cff9012272814b16d
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-21 15:26:57 -0500

    added comment on faq exercises

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -9,12 +9,13 @@ $( "dt" ).click(function() {
 	$( this ).css( "background-color", "yellow" )
 });
 
-
+//when the element with the id of toggle new is clicked the last item in the
+//list will be toggled classes to the value of highlight
 $( "#toggle-new" ).click(function() {
 	$( "li:nth-child(4)" ).toggleClass( "highlight" );
 });
 
-
+//whenever an h3 element is clicked all the li elements will be toggled to a class of bold
 $( "h3" ).click(function() {
 	$( "li" ).toggleClass( "bold" );
 });
@@ -22,6 +23,5 @@ $( "h3" ).click(function() {
 
 $( "li" ).click(function() {
 	$('li:first-child').css('background-color', 'blue');
-
 });
 
