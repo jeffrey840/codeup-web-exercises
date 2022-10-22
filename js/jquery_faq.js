1ef77dcee4c4d5013786ece2238c00937212187c
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-22 15:22:20 -0500

    added iife/commented on jq faq

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,45 +1,45 @@
+(function () {
 
 //when the element with the id "toggle-c" is clicked, the dd element with class invisible will be toggled
-$( "#toggle-c" ).click(function() {
-	$("dd" ).toggleClass( "invisible" );
-});
+	$("#toggle-c").click(function () {
+		$("dd").toggleClass("invisible");
+	});
 
 //when clicked the dt element will turn yellow
-$( "dt" ).click(function() {
-	$( this ).css( "background-color", "yellow" )
-});
+	$("dt").click(function () {
+		$(this).css("background-color", "yellow")
+	});
 
 //when the element with the id of toggle new is clicked the last item in the
 //list will be toggled classes to the value of highlight
-$( "#toggle-new" ).click(function() {
-	$( "li:nth-child(4)" ).toggleClass( "highlight" );
-});
+	$("#toggle-new").click(function () {
+		$("li:nth-child(4)").toggleClass("highlight");
+	});
 
 //whenever an h3 element is clicked all the li elements will be toggled to a class of bold
-$( "h3" ).click(function() {
-	$( "li" ).toggleClass( "bold" );
-});
+	$("h3").click(function () {
+		$("li").toggleClass("bold");
+	});
 
 
-$( "li" ).click(function() {
-	$('li:first-child').css('background-color', 'blue');
-});
+	$("li").click(function () {
+		$('li:first-child').css('background-color', 'blue');
+	});
 
 //==================== BONUS ===================//
 
-
-	// Painting Shuffle
-	// Determines the behavior of the left painting
-	$("div").children().first().on("click", function () {
+// Determines the behavior of the left painting
+//	if the left image is clicked it will switch with the center image
+$("div").children().first().on("click", function () {
 		let centerPainting = $("#center").attr("src");
 		let leftPainting = $("#left").attr("src");
 		$("#center").attr("src", leftPainting);
 		$("#left").attr("src", centerPainting);
 	});
 
-	// Randomly produces a 0 or 1 then based on that value
-	// determines the behavior of the center painting
-	$("div").children().first().next().on("click", function () {
+// Randomly produces a 0 or 1 then based on that value
+// determines the behavior of the center painting
+$("div").children().first().next().on("click", function () {
 		let randomNumber = Math.floor(Math.random() * 2);
 		let centerPainting = $("#center").attr("src");
 		if (randomNumber === 0) {
@@ -53,12 +53,13 @@ $( "li" ).click(function() {
 		}
 	});
 
-	// Determines the behavior of the right painting
-	$("div").children().last().on("click", function () {
+// Determines the behavior of the right painting
+//	if the right image is clicked it will switch with the center image
+$("div").children().last().on("click", function () {
 		let centerPainting = $("#center").attr("src");
 		let rightPainting = $("#right").attr("src");
 		$("#center").attr("src", rightPainting);
 		$("#right").attr("src", centerPainting);
 	});
 
-
+})();
