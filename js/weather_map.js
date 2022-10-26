ca20e8def3eeb0a23d1a5b97bc917c91e980fbbb
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-25 21:12:12 -0500

    added comments

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,7 @@
 
 
-
+//when the the user moves the marker to a new location the marker will update the cards
+//whne the user input a location in the textbox find that location update cards
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={29.423017}&lon={-98.48527}&exclude={hourly,daily}&appid={"91153175f66a5d3b4967f618fd8e6102"}
 
@@ -10,14 +11,16 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 	lon:   -98.48527,
 	units: "imperial"
 }).done(function(data) {
-	console.log('5 day forecast', data);
+	console.log('5 day forecast', data.list[0]['weather']);
 
 });
+
 
 mapboxgl.accessToken = "pk.eyJ1IjoiamVmZnJleTg0MCIsImEiOiJjbDluNTZtY2QwNGRxM29sN29lMjZkdDVjIn0.uIVExK3ADOwPLi3QpTx7Ug";
 const map = new mapboxgl.Map({
 	container: 'map', // container ID
 	style: 'mapbox://styles/mapbox/streets-v11', // style URL
-	center: [-117.60784696753407,33.57883646511674], // starting position [lng, lat]
+	center: [-98.48527,29.423017], // starting position [lng, lat]
 	zoom: 9 // starting zoom
 });
+
