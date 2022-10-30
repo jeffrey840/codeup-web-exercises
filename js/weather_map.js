0593b823f60d719ca682dc526e0224a6d413be6c
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-30 12:45:02 -0500

    changed comments

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,6 @@
 
-
 // setting up global variables to call the data from Weather Map
+//latitude and longitude are  set to Dallas
 let longitude = -96.7969
 let latitude = 32.7763
 
@@ -10,7 +10,6 @@ getData();
 // ------- Query the Weather Map API ----------------------------------------------
 
 // String and Object Method:
-// Using this method for the sake of readability
 // Excluding minutely and hourly.
 // Changing kelvins to imperial: Fahrenheit
 // Defining the 'get' request as a function 'getData' in order to call it multiple times
@@ -21,21 +20,20 @@ function getData() {
 		lon: longitude,
 		units: "imperial",
 		exclude: "minutely, hourly",
-		zoom: 14
 	}).done(function (data) {
-		console.log(data)
-		handleResponse(data)
+		// console.log(data)
+		//calling the response function.
+		response(data)
 	});
 }
 
-// Setting up handle response to iterate through the returned data and populate the html:
-function handleResponse(data) {
+// Setting up response to iterate through the returned data from the api and populate the html:
+function response(data) {
 	let days = data.daily;
 	let html = "";
 	for (let i = 0; i < 5; i++) {
 		let date = dateMaker(i);
 		let iconCode = days[i].weather[0].icon;
-		cardColor(iconCode);
 		let tempHigh = Math.round(days[i].temp.max);
 		let tempLow = Math.round(days[i].temp.min);
 		let hum = days[i].humidity;
@@ -49,7 +47,7 @@ function handleResponse(data) {
 		itemHtml += "<img style='height: 50px; width: 50px;' src='http://openweathermap.org/img/w/" + iconCode + ".png'>"
 		itemHtml += '<h5 class="highText">' + 'High ' + tempHigh + '</h5>';
 		itemHtml += '<h5 class="lowText">' + 'Low ' + tempLow + '</h5>';
-		itemHtml += '<p class="card-footer my-3">' + description + '</p>';
+		itemHtml += '<p class="card-footer my-1">' + description + '</p>';
 		itemHtml += '<p class="card-footer my-1">' + 'humidity: '  + hum + '</p>';
 		itemHtml += '<p class="card-footer my-1">' + 'wind: '  + wnd + '</p>';
 		itemHtml += '<p class="card-footer my-1">' + 'pressure: '  + press + '</p>';
@@ -58,14 +56,13 @@ function handleResponse(data) {
 	}
 	$('#insertWeatherBoxes').html(html);
 
-	//------- Map -----------------------------------------------------
-
+	//------- Map -----------------
 	mapboxgl.accessToken = MAPBOX_API_KEY
 	const map = new mapboxgl.Map({
 		container: 'map', // container ID
 		style: 'mapbox://styles/mapbox/dark-v10', // style URL
 		center: [longitude, latitude], // starting position [lng, lat]
-		zoom: 9 // starting zoom
+		zoom: 10 // starting zoom
 	});
 
 //Starting Draggable Marker (default point)
@@ -77,22 +74,22 @@ function handleResponse(data) {
 		.addTo(map);
 
 // Adding functionality to draggable marker
-
 	function onDragEnd() {
 		let lngLat = marker.getLngLat();
 		longitude = lngLat.lng;
 		latitude = lngLat.lat;
 		getData();
 	}
-
 	marker.on('dragend', onDragEnd);
 }
 
-// Pass the dateMaker function the index number (i) in the handleResponse.
+
+// Pass the dateMaker function the index number (i) in the response function.
 // The setDate() method sets the day of the Date object relative
 // to the beginning of the currently set month. -MDN
 // The getDate() method returns the day of the month for the specified
 // date according to local time. -MDN
+//range from 1-31.
 
 function dateMaker(num) {
 	let date = new Date();
@@ -100,32 +97,8 @@ function dateMaker(num) {
 	return date.toDateString().slice(0, 10);
 }
 
-// Change the background of the Weather Cards depending on the iconCode
-function cardColor(code) {
-	$('.card').css("background-image", function () {
-			if (code === "01d") {
-				return "linear-gradient(45deg, skyblue, skyblue)";
-			}
-			if (code === "02d" || "03d" || "10d") {
-				return "linear-gradient(45deg, skyblue, grey)";
-			}
-			if (code === "04d" || "09d" || "11d") {
-				return "linear-gradient(45deg, lightgray, darkslategrey)";
-			}
-			if (code === "50d") {
-				return "linear-gradient(45deg, gray, ghostwhite)";
-			}
-			if (code === "13d") {
-				return "linear-gradient(45deg, lightgrey, whitesmoke)";
-			} else {
-				return "linear-gradient(45deg, skyblue, grey)";
-			}
 
-		}
-	)
-}
-
-//------- Search by City geocode ----------------------------------------
+//------- Search by City geocode ------------
 
 $(".btn").click(function (e) {
 	e.preventDefault()
