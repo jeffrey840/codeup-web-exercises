24502b6e7d9cf137272745e4bfaaba9765997f06
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-30 21:45:56 -0500

    worked on some todo problems

--- js/es6.js
+++ js/es6.js
@@ -28,36 +28,40 @@ const users = [
     languages: ['html', 'css', 'javascript', 'php']
   }
 ];
+console.log(users)
 
 // TODO: fill in your name and email and add some programming languages you know
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'jeffrey';
+const email = 'jeffrey@codeup.com';
+const languages = ['html', 'css', 'javascript', 'jquery', 'bootstrap'];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-users.forEach(function(user) {
-  return names.push(user.name);
-});
+// users.forEach(function(user) {
+//   return emails.push(user.email);
+// });
+// users.forEach(function(user) {
+//   return names.push(user.name);
+// });
+
+users.forEach(user => emails.push(user.email));
+users.forEach(user => names.push(user.name));
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
